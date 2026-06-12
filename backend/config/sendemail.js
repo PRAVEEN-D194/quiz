9029515aa7ee77bc82130f1806700c6339a355a7
PRAVEEN-D194 <praveend12321@gmail.com>
2026-06-12 19:19:05 +0530

    email send problem soved

--- backend/config/sendemail.js
+++ backend/config/sendemail.js
@@ -3,17 +3,14 @@ require("dotenv").config();
 
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail', // Specifying 'gmail' allows Nodemailer to optimize the configurations
-  host: 'smtp.gmail.com',
-  port: 465,        // Try switching to port 465...
-  secure: true,     // ...and set secure to true. (Render sometimes prefers 465 over 587)
+   host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, 
+  localAddress: '0.0.0.0', // Forces Node to use IPv4 instead of IPv6 (:::0)
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // Must be the 16-digit App Password
-  },
-  connectionTimeout: 10000, // 10 seconds timeout
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
+    user: 'your-email@gmail.com',
+    pass: 'your-password'
+  }
 })
 
 module.exports = transporter
